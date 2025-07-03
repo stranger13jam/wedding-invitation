02fe4a960ba523980e51e420f3a46c2057ecaf09
Alex Sidorov <stranger13jam@gmail.com>
2025-07-03 21:30:10 +0300

    Initial commit

--- app/pages/blocks/invitation/questionnaire.py
+++ app/pages/blocks/invitation/questionnaire.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit.components.v1 import iframe
 
 
 def render():
@@ -12,50 +13,57 @@ def render():
       )
 
       
-      with st.form(key="questionnaire_form"):
-            st.markdown(
-                  unsafe_allow_html=True,
-                  body="""
-                  <div class="title">
-                        Пожалуйста, подтвердите своё присутствие на свадьбе <strong>до</strong>:
-                        <h4>1 сентября 2025 г.</h4>
-                  </div>
-                  """
-            )
+      # with st.form(key="questionnaire_form"):
+      #       st.markdown(
+      #             unsafe_allow_html=True,
+      #             body="""
+      #             <div class="title">
+      #                   Пожалуйста, подтвердите своё присутствие на свадьбе <strong>до</strong>:
+      #                   <h4>1 сентября 2025 г.</h4>
+      #             </div>
+      #             """
+      #       )
 
-            st.text_input(
-                  label="Имя и фамилия",
-                  placeholder="Введите ваше имя и фамилию",
-                  key="questionnaire_name"
-            )
+      #       st.text_input(
+      #             label="Имя и фамилия",
+      #             placeholder="Введите ваше имя и фамилию",
+      #             key="questionnaire_name"
+      #       )
 
-            st.radio(
-                  label="Вы будете на свадьбе?",
-                  options=["Да", "Нет"],
-                  format_func=lambda option: "Да, конечно" if option == "Да" else "Нет, к сожалению",
-                  index=0,
-                  key="questionnaire_will_come"
-            )
+      #       st.radio(
+      #             label="Вы будете на свадьбе?",
+      #             options=["Да", "Нет"],
+      #             format_func=lambda option: "Да, конечно" if option == "Да" else "Нет, к сожалению",
+      #             index=0,
+      #             key="questionnaire_will_come"
+      #       )
 
-            st.text_input(
-                  label="Имя и фамилия вашего партнера",
-                  placeholder="Введите имя и фамилию вашего партнера, если вы будете не один/не одна",
-                  key="questionnaire_partner_name"
-            )
+      #       st.text_input(
+      #             label="Имя и фамилия вашего партнера",
+      #             placeholder="Введите имя и фамилию вашего партнера, если вы будете не один/не одна",
+      #             key="questionnaire_partner_name"
+      #       )
 
-            st.text_area(
-                  label="Ваши пожелания или вопросы",
-                  placeholder="Напишите ваши пожелания или вопросы",
-                  key="questionnaire_wishes"
-            )
+      #       st.text_area(
+      #             label="Ваши пожелания или вопросы",
+      #             placeholder="Напишите ваши пожелания или вопросы",
+      #             key="questionnaire_wishes"
+      #       )
 
-            if st.form_submit_button(
-                  label="Отправить",
-                  type="primary",
-                  use_container_width=True
-            ):
-                  if st.session_state["questionnaire_name"] and st.session_state["questionnaire_will_come"]:
-                        # save_answer()
-                        st.success("Анкета отправлена")
-                  else:
-                        st.warning("Пожалуйста, укажите ваши имя и фамилию")
+      #       if st.form_submit_button(
+      #             label="Отправить",
+      #             type="primary",
+      #             use_container_width=True
+      #       ):
+      #             if st.session_state["questionnaire_name"] and st.session_state["questionnaire_will_come"]:
+      #                   # save_answer()
+      #                   st.success("Анкета отправлена")
+      #             else:
+      #                   st.warning("Пожалуйста, укажите ваши имя и фамилию")
+
+
+      iframe(
+            src="https://docs.google.com/forms/d/e/1FAIpQLSey60ENX0HRIQNg0ZWG3ghQu0D5wF-We-lrITbR5IZVhZ9kaA/viewform?embedded=true",
+            height=600,
+            scrolling=True
+      )
